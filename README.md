343e084378ec1d4b6dd406e72a19f48664fa06e3
unknown <babbili@protonmail.com>
2022-02-07 10:43:15 +0100

    adding text animation

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Space Rocket 
 I draw the illustration in InkScape and animated it using SVG ```<animate />``` element
-HTML & CSS
+Javascript text splitting & CSS animation
--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 let animate = document.getElementById('animate')
 let letters = animate.textContent.split('')
+console.log(letters)
 animate.innerHTML=''
 letters.forEach((letter)=> {
     animate.innerHTML += `<span class="animate-s">${letter}</span>`
@@ -8,7 +9,9 @@ const letterSpans = [...document.querySelectorAll('.animate-s')]
 letterSpans.forEach((letterSpan, idx) => {
     setTimeout(() => {
         letterSpan.classList.add('active')
-    }, idx*100)
+    }, idx*130)
 })
 const heroText = document.getElementById('hero-text')
-heroText.classList.add('animate-r')
+setTimeout(() => {
+    heroText.classList.add('animate-r')
+}, 1000)
